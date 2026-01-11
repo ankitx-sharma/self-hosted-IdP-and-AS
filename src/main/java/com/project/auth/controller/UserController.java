bbf977dc5907b7d5d0aac9a2ec97022858d4b9b8
Ankita Sharma <ankitasharma.281993@gmail.com>
2026-01-11 18:12:19 +0100

    Append -> added new api calls for admin role

--- src/main/java/com/project/auth/controller/UserController.java
+++ src/main/java/com/project/auth/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.auth.dto.TokenResponse;
 import com.project.auth.dto.User;
 import com.project.auth.service.UserService;
-import com.project.auth.util.JwtUtil;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -24,13 +22,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private JwtUtil jwtUtil;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@PostMapping("/register")
+	@PostMapping("/register/user")
 	public ResponseEntity<String> registerUser(@RequestBody User user) {
 		Optional<User> userData = userService.findByUserName(user.getUsername());
 		
@@ -41,19 +33,30 @@ public class UserController {
 		return ResponseEntity.ok("User Registered");
 	}
 	
+	@PostMapping("/register/admin")
+	public ResponseEntity<String> registerAdmin(@RequestBody User user) {
+		Optional<User> userData = userService.findByUserName(user.getUsername());
+		
+		if(userData.isPresent()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body("User already registered.");
+		}
+		userService.registerAdmin(user);
+		return ResponseEntity.ok("User Registered");
+	}
+	
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody User loginRequest){
-		Optional<User> userOptional = userService.findByUserName(loginRequest.getUsername());
+		String token = userService.loginUser(loginRequest);
 		
-		if(userOptional.isPresent()) {
-			User user = userOptional.get();
-			if(passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-				String token = jwtUtil.generateAccessToken(loginRequest.getUsername());
-				return ResponseEntity.status(HttpStatus.ACCEPTED.value()).body(new TokenResponse(token));
-			}
-		}
+		if(token != null)
+			return ResponseEntity.status(HttpStatus.ACCEPTED.value()).body(new TokenResponse(token));
 		
 		return ResponseEntity.status(401).body("Invalid username or password");
+	}
+	
+	@GetMapping("/profile/admin")
+	public ResponseEntity<?> getAdminProfile(){
+		return ResponseEntity.ok("Access Granted to Admin. Secured profile info!");
 	}
 	
 	@GetMapping("/profile")
